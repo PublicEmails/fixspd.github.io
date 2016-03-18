27c3b8023eec52d2194f37c93d1076de96641eb0
Tim Clemans <tim@insideyourgovernment.com>
2016-03-17 20:03:21 -0700

    Create webmaster_toolkit_for_socrata.js

---
+++ js/webmaster_toolkit_for_socrata.js
@@ -0,0 +1,57 @@
+/*
+Add <script src="https://fixspd.org/js/webmaster_toolkit_for_socrata.js"></script> to your webpage
+To add a count(*) add something similar to
+<p>There are <span class="socrata_count" data-url="https://data.seattle.gov/resource/a4j2-uu8v.json?$select=count(*)&$where=completed_date IS NULL"></span> open Seattle Police records requests.</p>
+
+*/
+if (typeof console == "undefined") {
+    this.console = {log: function() {}};
+}
+
+function updateItemWithSimpleCount(item, data) {
+  item.text(data[0][Object.keys(data[0])[0]]);
+}
+
+function handleSimpleCount() {
+  $.each($('.socrata_count'), function(item) {
+    console.log($(this).attr('data-url'))
+    var socrataUrl = $(this).attr('data-url');
+    var item = $(this)
+    $.get(socrataUrl, function(data) {
+      updateItemWithSimpleCount(item, data);
+    });
+  });
+}
+
+function main() {
+    
+    handleSimpleCount();
+}
+
+if (!window.jQuery) {
+// Anonymous "self-invoking" function
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 100);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        main();
+    });
+})();
+} else {
+    main();
+}
