0cd28aee8b87a9dad08def3f6e41413f4aa64040
Tim Clemans <tim@insideyourgovernment.com>
2016-03-25 21:23:07 -0700

    Update webmaster_toolkit_for_socrata.js

--- js/webmaster_toolkit_for_socrata.js
+++ js/webmaster_toolkit_for_socrata.js
@@ -41,7 +41,7 @@ function handleSimpleCountsSum() {
   });
 }
 function main() {
-    var plugins = ['https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'];
+    var plugins = ['https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'];
     $.each(plugins, function(i,url){
         if (url.endsWith('.js')) {
             var script = document.createElement("SCRIPT");
